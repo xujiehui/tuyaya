a42027303da28eff80e161c165c28523d08857da
xujiehui <xujiehui@users.noreply.github.com>
2024-12-04 09:08:08 +0000

    deploy: 512542754feb308088bc3ead1e3bec98d06d91f0

---
+++ assets/index.3bbb390f.js
@@ -0,0 +1,1 @@
+import{d as o,D as r,C as a}from"./arco.92d13bd0.js";import{f as s,h as n}from"./vue.8b5801ee.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{i as default};
